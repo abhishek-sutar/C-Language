9a67529af57648e098f8939208a537d10c74fd11
abhishek-sutar <abhisheksutar1972@gmail.com>
2025-11-03 22:42:10 +0530

    Printing the Marks by array

---
+++ array1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int marks[4];
+    marks[0]=34;
+    printf("Enter the marks  %d\n", marks[0]);
+    marks[1]=56;  
+    printf("Enter the marks  %d\n", marks[1]);
+     marks[2]=566;
+    printf("Enter the marks  %d\n", marks[2]);
+     marks[3]=6;
+    printf("Enter the marks  %d\n", marks[3]);
+    return 0;
+}
